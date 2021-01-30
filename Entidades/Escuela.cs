7ec6390fe0f44b4509e8b462cc563e8f69461082
Vangi <dvangioni@gmail.com>
2021-01-29 22:04:46 -0300

    multiples constructores, parametros

--- Entidades/Escuela.cs
+++ Entidades/Escuela.cs
@@ -14,8 +14,20 @@ namespace corEscuela.Entidades
         public string Ciudad { get; set; }
         public TiposEscuela TipoEscuela { get; set; }
 
+        //Constructor
         public Escuela(string nombre, int año) => (Nombre, AñoDeCreación) = (nombre, año);
 
+        //Constructor
+        public Escuela(string nombre, int año, TiposEscuela tipo, string pais = "", string ciudad = "")
+        {
+            (Nombre, AñoDeCreación) = (nombre, año);
+            TipoEscuela = tipo;
+            Pais = pais;
+            Ciudad = ciudad;
+        }
+
+
+        //Override de ToString
         public override string ToString()
         {
             return $"Nombre: {Nombre} \nTipo: {TipoEscuela} \nPais: {Pais} \nCiudad: {Ciudad}";
--- Program.cs
+++ Program.cs
@@ -8,12 +8,15 @@ namespace corEscuela
         static void Main(string[] args)
         {
 
-            var escuela = new Escuela("Platzi Academy", 2021);
+            var escuela = new Escuela("Platzi Academy", 2012);
             escuela.Pais = "Colombia";
             escuela.Ciudad = "Bogotá";
             escuela.TipoEscuela = TiposEscuela.Primaria;
 
             Console.WriteLine(escuela);
+
+            var escuela2 = new Escuela("Platzi Academy", 2021, TiposEscuela.Secundaria, ciudad: "Santa Fe", pais: "Argentina");
+            Console.Write(escuela2);
         }
     }
 }
